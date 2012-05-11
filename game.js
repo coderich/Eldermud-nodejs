232c4c1e0e3bcf2d10e30a24c8217a8840aa9ae6
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-11 11:19:14 -0400

    updates

---
+++ game.js
@@ -0,0 +1,43 @@
+var app = require('express').createServer();
+var io = require('socket.io').listen(app);
+
+require('jade');
+app.set('view engine', 'jade');
+app.set('view options', {
+	layout : false
+});
+
+app.get('/*.(js|css)', function(req, res) {
+	res.sendfile('./public' + req.url);
+});
+
+app.get('/', function(req, res) {
+	res.render('game');
+});
+
+var clients = {};
+
+io.sockets.on('connection', function(client) {
+	// Add client to list and give welcome
+	clients[client.id] = client;
+	client.send("Hello, Welcome to ElderMud!");
+	client.broadcast.send("Someone has just entered the room, say hello!");
+	io.sockets.emit('who', {
+		who : Object.keys(clients)
+	});
+
+	client.on('disconnect', function() {
+		delete clients[client.id];
+		client.broadcast.send("Someone has just disconnected...");
+		io.sockets.emit('who', {
+			who : Object.keys(clients)
+		});
+	});
+
+	client.on('message', function(msg) {
+		client.send("You say: " + msg);
+		client.broadcast.send("Someone says: " + msg);
+	});
+});
+
+app.listen(8080);
---
+++ public/game.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	var console = $("#console");
+	var cmd = $("#cmd");
+	var who = $("#who");
+	var socket = io.connect();
+
+	// Send message to server
+	$(cmd).bind('keypress', function(e) {
+		var code = (e.keyCode ? e.keyCode : e.which);
+
+		if (code === 13) {
+			socket.send(cmd.val());
+			cmd.val("");
+		}
+	})
+
+	socket.on('connect', function() {
+		socket.on('message', function(msg) {
+			console.val(console.val() + msg + "\n");
+			console.scrollTop(console[0].scrollHeight - console.height());
+		});
+		socket.on('who', function(o) {
+			who.val(o.who.join("\n"));
+		});
+	});
+});
