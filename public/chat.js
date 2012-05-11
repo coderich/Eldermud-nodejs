f2c6a4ed31df53d055770f602fbfb019e079d0ac
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-11 00:31:30 -0400

    updates

--- public/chat.js
+++ public/chat.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
-	$console = $("#console");
-	$cmd = $("#cmd");
-
+	var console = $("#console");
+	var cmd = $("#cmd");
 	var socket = io.connect();
+
+	// Send message to server
+	$(cmd).bind('keypress', function(e) {
+		var code = (e.keyCode ? e.keyCode : e.which);
+
+		if (code === 13) {
+			socket.send(cmd.val());
+			cmd.val("");
+		}
+	})
+
 	socket.on('connect', function() {
 		socket.on('message', function(msg) {
-			$($console).val($($console).val() + msg + "\n");
+			console.val(console.val() + msg + "\n");
 		});
 	});
 });
