227fef0efc14d306f23a5834fb95fee67af095e5
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-10 21:45:30 -0400

    Initial dupe of chat

---
+++ chat.js
@@ -0,0 +1,33 @@
+var app = require('express').createServer();
+var socket = require('socket.io').listen(app);
+
+require('jade');
+app.set('view engine', 'jade');
+app.set('view options', {
+	layout : false
+});
+
+app.get('/*.(js|css)', function(req, res) {
+	res.sendfile('./public' + req.url);
+});
+
+app.get('/', function(req, res) {
+	res.render('chat');
+});
+
+socket.sockets.on('connection', function(client) {
+	socket.sockets.json.send({
+		clients : socket.sockets.length
+	});
+	client.on('disconnect', function() {
+		clientDisconnect(client)
+	});
+});
+
+function clientDisconnect(client) {
+	socket.sockets.json.send({
+		clients : socket.sockets.length
+	});
+}
+
+app.listen(8080);
