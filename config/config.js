1b21d52abb7be7acb571934cabd6d9935cddef08
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-12 17:03:56 -0400

    stuff

--- config/config.js
+++ config/config.js
@@ -1,24 +1,27 @@
-module.exports = {
-	constants : {
+(function() {
+	var constants = {
 		CMD_ATTACK : 1,
-		CMD_EAST : 2,
-		CMD_WEST : 3,
-		CMD_NORTH : 4,
-		CMD_SOUTH : 5,
+		CMD_NORTH : 2,
+		CMD_SOUTH : 3,
+		CMD_EAST : 4,
+		CMD_WEST : 5,
 		CMD_NORTHEAST : 6,
 		CMD_NORTHWEST : 7,
 		CMD_SOUTHEAST : 8,
 		CMD_SOUTHWEST : 9
-	},
-	aliases : {
-		1 : [ 'attack' ],
-		2 : [ 'east' ],
-		3 : [ 'west' ],
-		4 : [ 'north' ],
-		5 : [ 'south' ],
-		6 : [ 'northeast', 'ne' ],
-		7 : [ 'northwest', 'nw' ],
-		8 : [ 'southeast', 'se' ],
-		9 : [ 'southwest', 'sw' ]
-	}
-};
+	};
+
+	var aliases = new Object();
+	aliases[CMD_ATTACK] = [ 'attack' ];
+	aliases[CMD_NORTH] = [ 'north' ];
+	aliases[CMD_SOUTH] = [ 'south' ];
+	aliases[CMD_EAST] = [ 'east' ];
+	aliases[CMD_WEST] = [ 'west' ];
+	aliases[CMD_NORTHEAST] = [ 'northeast', 'ne' ];
+	aliases[CMD_NORTHWEST] = [ 'northwest', 'nw' ];
+	aliases[CMD_SOUTHEAST] = [ 'southeast', 'se' ];
+	aliases[CMD_SOUTHWEST] = [ 'southwest', 'sw' ];
+
+	exports.constants = constants;
+	exports.aliases = aliases;
+})()
