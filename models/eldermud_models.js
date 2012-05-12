bc407e364dad13242dc1782f46182569e764dfcc
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-12 02:48:59 -0400

    changes

--- models/eldermud_models.js
+++ models/eldermud_models.js
@@ -20,13 +20,15 @@
 				throw "AppModel InvalidConstructorArgs";
 			}
 			args.players.on('add', function(player) {
-				player.get('socket').send("Hello, Welcome to ElderMud!");
-				player.get('socket').broadcast.send("Seomeone has just entered the game!");
+				var socket = player.get('socket');
+				socket.send("Hello, Welcome to ElderMud!");
+				socket.broadcast.send("Seomeone has just entered the game!");
 			});
 			args.players.on('remove', function(player) {
 				args.io.sockets.send("Someone has just left the game...");
 			});
 			args.players.on('change:room', function(room) {
+				console.log("CHANGE ROOM");
 			});
 		}
 	});
