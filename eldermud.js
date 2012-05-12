ec8409a30a49447262398058e45168b9ac6de28a
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-12 02:05:47 -0400

    changes

---
+++ eldermud.js
@@ -0,0 +1,33 @@
+var server = require('express').createServer();
+var _ = require('underscore')._;
+var backbone = require('backbone');
+var io = require('socket.io').listen(app);
+var models = require('./models/eldermud_models');
+
+require('jade');
+server.set('view engine', 'jade');
+server.set('view options', {
+	layout : false
+});
+
+server.get('/*.(js|css)', function(req, res) {
+	res.sendfile('./public' + req.url);
+});
+
+server.get('/', function(req, res) {
+	res.render('eldermud_view');
+});
+
+var rooms = new models.RoomCollection([new models.Room({id:1, e:2}), new models.Room({id:2, w:1})]);
+var map = new models.Map({rooms: rooms});
+var players = new models.PlayerCollection();
+var app = new models.AppModel({io: io, map: map, players: players});
+
+io.sockets.on('connection', function(client) {
+	client.on('disconnect', function() {});
+
+	// Add client to a random room...
+	players.add({room: rooms.at(Math.floor((Math.random() * 2) + 1)), socket: client});
+});
+
+app.listen(8080);
---
+++ models/eldermud_models.js
@@ -0,0 +1,58 @@
+(function() {
+	// This trick here let's us figure out if we're being used as a "CommonJS"
+	// module or just in a script tag This allows for a module that works on the
+	// server as well as the client
+	var server = false, models;
+	if (typeof exports !== 'undefined') {
+		models = exports;
+		server = true;
+	} else {
+		models = this.models = {};
+	}
+
+	// The ApplicationModel that holds the entire state
+	models.AppModel = Backbone.Model.extend({
+		initialize : function(args) {
+			if (!args || !args.io || !args.map || !args.players) {
+				throw "InvalidConstructorArgs";
+			}
+			this.players.on('add', function(player) {
+				player.get('socket').send("Hello, Welcome to ElderMud!");
+			});
+			this.players.on('change:room', function(room) {
+				
+			});
+		}
+	});
+
+	// The map of the world
+	models.Map = Backbone.Model.extend({
+		initialize : function() {
+			this.rooms = new models.RoomCollection();
+		}
+	});
+
+	// A Player model
+	models.Player = Backbone.Model.extend({
+		initialize : function(args) {
+			if (!args || !args.room) {
+				throw "InvalidConstrutorArgs";
+			}
+		}
+	});
+	models.PlayerCollection = Backbone.Collection.extend({
+		model : models.Player
+	});
+
+	// A Room model
+	models.Room = Backbone.Model.extend({
+		initialize : function(args) {
+			if (!args || !args.title || !args.description) {
+				throw "InvalidConstructorArgs";
+			}
+		}
+	});
+	models.RoomCollection = Backbone.Collection.extend({
+		model : models.Room
+	});
+});
