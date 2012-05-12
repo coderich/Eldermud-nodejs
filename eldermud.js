58f89ed5a52a560652c117e4554495b4787692f7
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-12 02:15:21 -0400

    changes

--- eldermud.js
+++ eldermud.js
@@ -18,7 +18,9 @@ server.get('/', function(req, res) {
 	res.render('eldermud_view');
 });
 
-var rooms = new models.models.RoomCollection([{id:1, e:2}, {id:2, w:1}]);
+var room1 = new models.Room({id:1, e:2});
+var room2 = new models.Room({id:2, w:1});
+var rooms = new models.RoomCollection([room1, room2]);
 var map = new models.Map({rooms: rooms});
 var players = new models.PlayerCollection();
 var app = new models.AppModel({io: io, map: map, players: players});
