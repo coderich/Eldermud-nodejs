9f43052fbbfcf9614a10e3e85968bf91744ac4f9
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-13 00:20:17 -0400

    stuff

--- models/Data.js
+++ models/Data.js
@@ -2,29 +2,23 @@ var _ = require('underscore')._;
 var Backbone = require('backbone');
 
 var Map = require('./Map');
-var PlayerCollection = requre('./PlayerCollection');
+var PlayerCollection = require('./PlayerCollection');
 
 module.exports = Backbone.Model.extend({
 	defaults : {
 		map : new Map(),
 		players : new PlayerCollection()
 	}
-	
-	/*
-	initialize : function(args) {
-		if (!args || !args.map || !args.players) {
-			throw "AppModel InvalidConstructorArgs";
-		}
 
-		args.players.on('add', function(player) {
-			var socket = player.get('socket');
-			socket.send("Hello, Welcome to ElderMud!");
-			socket.broadcast.send("Seomeone has just entered the game!");
-		});
-
-		args.players.on('remove', function(player) {
-			args.io.sockets.send("Someone has just left the game...");
-		});
-	}
-	*/
+/*
+ * initialize : function(args) { if (!args || !args.map || !args.players) {
+ * throw "AppModel InvalidConstructorArgs"; }
+ * 
+ * args.players.on('add', function(player) { var socket = player.get('socket');
+ * socket.send("Hello, Welcome to ElderMud!"); socket.broadcast.send("Seomeone
+ * has just entered the game!"); });
+ * 
+ * args.players.on('remove', function(player) { args.io.sockets.send("Someone
+ * has just left the game..."); }); }
+ */
 });
