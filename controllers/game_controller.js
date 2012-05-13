9f103dda9e88e78cd1a4bff52979bcb93287f5ed
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-12 23:51:02 -0400

    stuff

--- controllers/game_controller.js
+++ controllers/game_controller.js
@@ -4,21 +4,33 @@ var Backbone = require('backbone');
 module.exports = {
 	// IO Handling
 	ioConnection : function(socket) {
+		// Disconnect handler
+		socket.on('disconnect', function() {
+			module.exports.trigger('ioServerToAll', {
+				'msg' : 'Someone has just disconnected...'
+			});
+		});
+		
+		// Message handler
+		socket.on('message', function(msg) {
+			module.exports.trigger('ioSocketToAll', {
+				socket : this,
+				msg : msg
+			});
+		});
+
+		// Welcome the new player
 		module.exports.trigger('ioServerToSockets', {
 			sockets : [ socket ],
 			msg : 'Hello, Welcome to ElderMud!!!'
 		});
+		
+		// Notifiy everyone else of the new arrival
 		module.exports.trigger('ioSocketToAll', {
 			socket : socket,
 			'msg' : 'Someone has just entered the game!'
 		});
 	},
-	ioDisconnect : function() {
-
-	},
-	ioMessage : function(msg) {
-
-	}
 };
 
 // Extend to be an event dispatcher
--- eldermud.js
+++ eldermud.js
@@ -4,8 +4,6 @@ var game = require('./controllers/game_controller');
 
 // We delegate inbound IO to the game controller
 io.sockets.on('connection', game.ioConnection);
-io.sockets.on('disconnect', game.ioDisconnect);
-io.sockets.on('message', game.ioMessage);
 
 // We handle outbound IO from the game controler here
 game.on('ioServerToSockets', function(e) {
@@ -32,18 +30,3 @@ game.on('ioSocketToAll', function(e) {
 
 // Start the server
 server.listen(8080);
-
-/*
- * io.sockets.on('connection', function(socket) { // Add client to a random
- * room... var player = new models.Player({socket:socket});
- * player.set({room:rooms.at(0)}); players.add(player);
- * 
- * socket.on('disconnect', function() { players.remove(player); });
- * 
- * socket.on('message', function(msg) { var words = msg.split(" "); var cmd =
- * helper.getCommand(words[0]);
- * 
- * switch (cmd) { default:
- * 
- * break; } }); });
- */
