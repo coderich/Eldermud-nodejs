4feb59f9a751c2b05a275f34ab5df184e174c5d7
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-14 16:47:51 -0400

    chchchanges

--- models/Map.js
+++ models/Map.js
@@ -3,9 +3,9 @@ var Backbone = require('backbone');
 
 module.exports = Backbone.Model.extend({
 	initialize : function(args) {
-		for (var id in args.room) {
-			for (var p in args.room[id].exits) {
-				args.room[id].exits[p] = args.room[args.room[id].exits[p]];
+		for (var id in args.rooms) {
+			for (var p in args.rooms[id].exits) {
+				args.rooms[id].exits[p] = args.rooms[args.rooms[id].exits[p]];
 			}
 		}
 	}
