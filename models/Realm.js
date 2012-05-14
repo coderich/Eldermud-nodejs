8f94126a667d4cfa0f954c600003bbc1bbed6d16
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-14 17:01:01 -0400

    chchchanges

--- models/Realm.js
+++ models/Realm.js
@@ -1,6 +1,9 @@
 var _ = require('underscore')._;
 var Backbone = require('backbone');
 
+var Map = require('./Map');
+var PlayerCollection = require('./PlayerCollection');
+
 module.exports = Backbone.Model.extend({
 	defaults : {
 		map : new Map(),
