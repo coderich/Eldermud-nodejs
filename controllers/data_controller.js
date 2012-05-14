6de4277310d2f38f9adb9d0139ca225f1ad735a2
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-14 14:23:26 -0400

    stuff

--- controllers/data_controller.js
+++ controllers/data_controller.js
@@ -17,7 +17,7 @@ module.exports = {
 	},
 	load : function() {
 		var fs = require('fs');
-		var realm = fs.readFileSync('../data/realm.json', 'ascii');
+		var realm = fs.readFileSync('./data/realm.json', 'ascii');
 		console.log(realm);
 		return new Realm(realm);
 	}
