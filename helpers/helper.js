c13d36f95721b4db96eee03124e61161fd438a30
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-15 01:21:46 -0400

    chchchanges

---
+++ helpers/helper.js
@@ -0,0 +1,18 @@
+var config = require('../config/config.js');
+
+module.exports = {
+	getCommand : function(word) {
+		word = word.lowercase();
+		var aliases = config.aliases;
+
+		for ( var cmd in aliases) {
+			for ( var i = 0; i < aliases[cmd].length; i++) {
+				if (aliases[cmd][i] === word) {
+					return cmd;
+				}
+			}
+		}
+
+		return false;
+	}
+};
