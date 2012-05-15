bea7dd7b62bbcf1913b15c82728ac11856b0f094
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-14 23:01:30 -0400

    chchchanges

--- controllers/game_controller.js
+++ controllers/game_controller.js
@@ -1,10 +1,16 @@
 var _ = require('underscore')._;
 var Backbone = require('backbone');
-var Player = require('../models/Player');
 var realm = require('./data_controller').load();
 
 // Every time a player is added
 realm.get('players').on('add', function(player) {
+	player.on('change:room', function() {
+		module.exports.trigger('ioServerToSockets', {
+			sockets : [ player.get('socket') ],
+			msg : 'You moved...'
+		});
+	});
+
 	module.exports.trigger('ioSocketToAll', {
 		socket : player.get('socket'),
 		msg : 'A new player has arrived!'
@@ -12,7 +18,7 @@ realm.get('players').on('add', function(player) {
 
 	module.exports.trigger('ioServerToSockets', {
 		sockets : [ player.get('socket') ],
-		msg : 'Welcome to Eldermud!'
+		msg : player.get('room').toString()
 	});
 });
 
@@ -35,20 +41,10 @@ module.exports = {
 		});
 
 		// TODO - Authenticate
-		var player = new Player();
-
-		player.on('change:room', function() {
-			module.exports.trigger('ioServerToSockets', {
-				sockets : [ player.get('socket') ],
-				msg : 'You moved...'
-			});
-		});
-
-		player.set({
+		realm.get('players').add({
 			room : 1,
 			socket : socket
 		});
-		realm.get('players').add(player);
 	}
 };
 
--- models/Room.js
+++ models/Room.js
@@ -1,4 +1,43 @@
 var _ = require('underscore')._;
 var Backbone = require('backbone');
 
-module.exports = Backbone.Model.extend({});
+module.exports = Backbone.Model.extend({
+	toString : function() {
+		var str = this.get('title') + "\n" + this.get('description') + "\n" + "Obvious Exists: ";
+		for ( var dir in this.get('exits')) {
+			switch (dir) {
+			case 'n':
+				str += 'north,';
+				break;
+			case 's':
+				str += 'south,';
+				break;
+			case 'e':
+				str += 'east,';
+				break;
+			case 'w':
+				str += 'west,';
+				break;
+			case 'ne':
+				str += 'northeast,';
+				break;
+			case 'nw':
+				str += 'northwest,';
+				break;
+			case 'se':
+				str += 'southeast,';
+				break;
+			case 'sw':
+				str += 'southwest,';
+				break;
+			case 'u':
+				str += 'up,';
+				break;
+			case 'd':
+				str += 'down,';
+				break;
+			}
+		}
+		return str.trim(',');
+	}
+});
