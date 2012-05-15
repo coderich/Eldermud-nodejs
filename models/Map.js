2c3308172c4660a860f0631f4a1e29e48cb0f613
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-15 00:42:07 -0400

    chchchanges

--- models/Map.js
+++ models/Map.js
@@ -3,9 +3,6 @@ var Backbone = require('backbone');
 var RoomCollection = require('./RoomCollection');
 
 module.exports = Backbone.Model.extend({
-	defaults : {
-		rooms : new RoomCollection()
-	},
 	initialize : function(args) {
 		if (args && args.rooms) {
 			this.unset('rooms');
