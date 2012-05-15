fc7d71cc80910f2a63c48e553704be4a01fdcf72
Richard Livolsi <richard.livolsi@gmail.com>
2012-05-14 23:13:25 -0400

    chchchanges

--- models/Room.js
+++ models/Room.js
@@ -3,44 +3,48 @@ var Backbone = require('backbone');
 
 module.exports = Backbone.Model.extend({
 	toString : function() {
-		var str = this.get('title') + "\n" + this.get('description') + "\n" + "Obvious Exists: ";
-		for ( var id in this.get('exits')) {
-			for ( var dir in this.get('exists')[id]) {
-				switch (dir) {
-				case 'n':
-					str = str + 'north,';
-					break;
-				case 's':
-					str += 'south,';
-					break;
-				case 'e':
-					str = str + 'east,';
-					break;
-				case 'w':
-					str = str + 'west,';
-					break;
-				case 'ne':
-					str += 'northeast,';
-					break;
-				case 'nw':
-					str += 'northwest,';
-					break;
-				case 'se':
-					str += 'southeast,';
-					break;
-				case 'sw':
-					str += 'southwest,';
-					break;
-				case 'u':
-					str += 'up,';
-					break;
-				case 'd':
-					str += 'down,';
-					break;
-				}
-			}
+		for ( var id in this) {
+			var room = this[id];
 			break;
 		}
+
+		var str = room.get('title') + "\n" + room.get('description') + "\n" + "Obvious Exists: ";
+
+		for ( var dir in room.get('exists')) {
+			switch (dir) {
+			case 'n':
+				str = str + 'north,';
+				break;
+			case 's':
+				str += 'south,';
+				break;
+			case 'e':
+				str = str + 'east,';
+				break;
+			case 'w':
+				str = str + 'west,';
+				break;
+			case 'ne':
+				str += 'northeast,';
+				break;
+			case 'nw':
+				str += 'northwest,';
+				break;
+			case 'se':
+				str += 'southeast,';
+				break;
+			case 'sw':
+				str += 'southwest,';
+				break;
+			case 'u':
+				str += 'up,';
+				break;
+			case 'd':
+				str += 'down,';
+				break;
+			}
+		}
+
 		return str.trim(',');
 	}
 });
